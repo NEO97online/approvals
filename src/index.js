13809f908bdb5d80b2b5f9ce37636fa9699688f6
Michael Auderer <mike@silverstone.io>
2020-07-21 16:33:22 -0400

    init commit

---
+++ src/index.js
@@ -0,0 +1,25 @@
+const core = require('@actions/core')
+const github = require('@actions/github')
+
+const octokit = new github.GitHub(process.env.GITHUB_TOKEN)
+
+const actor = github.context.actor
+const org = github.context.repository_owner
+const team_slug = core.getInput('team')
+
+async function main() {
+  try {
+    const res = await octokit.teams.listMembersInOrg({ org, team_slug })
+
+    const usersInTeam = res.data.map(user => user.login)
+    
+    if (!usersInTeam.includes(actor)) {
+      core.setFailed(`User ${actor} is NOT in team ${team_slug}`)
+    }
+  } catch (err) {
+    core.setFailed(err.message)
+  }
+}
+
+main()
+
